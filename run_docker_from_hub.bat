be12b773d8929a34d586456394d4172fb5fee86c
Georg Novotny <georg.novotny@aon.at>
2021-08-30 08:47:24 +0200

    Changes for jupyter notebook

--- run_docker_from_hub.bat
+++ run_docker_from_hub.bat
@@ -10,6 +10,6 @@ set pfad=%CD%\catkin_ws\src\
 echo +Path to catkin_ws=%pfad%
 echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-docker run -it --rm --network bridge  -p 5000:5000 --mount type=bind,source="%pfad%\",target=/home/fhtw_user/catkin_ws/src/fhtw/ --name "fhtw_ros-melodic" --privileged -e DISPLAY=%DISPLAY%  "georgno/fhtw-ros:melodic" "bash"
+docker run -it --rm --network host  --mount type=bind,source="%pfad%\",target=/home/fhtw_user/catkin_ws/src/fhtw/ --name "fhtw_ros-melodic" --privileged -e DISPLAY=%DISPLAY%  "georgno/fhtw-ros:melodic" "bash"
 
 cmd /k
