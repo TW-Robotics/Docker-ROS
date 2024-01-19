ede5ec46937d0cf8ed6dace82bbec23299465c85
Georg No <georg.novotny@aon.at>
2024-01-19 17:23:25 +0100

    Create docker_run_from_hub.bat

---
+++ docker_run_from_hub.bat
@@ -0,0 +1,16 @@
+@echo off
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo +Make sure you have a X11 server runing                                +
+echo +Make sure that "Native opengl" is disabled for the running X11 server +
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
+echo +Network IP: %NetworkIP%                                                 +
+set DISPLAY=%NetworkIP%:0.0
+echo +DISPLAY=%DISPLAY%                                                 +
+set pfad=%CD%\catkin_ws\src\
+echo +Path to catkin_ws=%pfad%
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+docker run -it --network bridge --rm -p 5000:5000 -p 8888:8888 --mount type=bind,source="%pfad%\",target=/home/fhtw_user/catkin_ws/src/fhtw/ --name "fhtw_ros" --privileged -e DISPLAY=%DISPLAY%  "georgno/fhtw-ros:mre2_ear_ws2023" "python3" "-m" "jupyterlab" "--ip='0.0.0.0'" "--no-browser"
+
+cmd /k
