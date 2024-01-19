7db51ae79c6a9818e8ce380de911626a133a2383
Georg No <georg.novotny@aon.at>
2024-01-19 17:23:56 +0100

    Create docker_run_from_hub.sh

---
+++ docker_run_from_hub.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+docker build -t "fhtw/ros-noetic:latest" --rm .
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/root/.Xauthority
+SHARED_DIR=/home/fhtw_user/catkin_ws/src/fhtw
+HOST_DIR=$(pwd)/catkin_ws/src
+
+echo -e "\e[32mMounting fodler:
+    $HOST_DIR    to
+    $SHARED_DIR\e[0m"
+
+docker run \
+    -it --rm \
+    --volume=$XSOCK:$XSOCK:rw \
+    --volume=$XAUTH:$XAUTH:rw \
+    --volume=$HOST_DIR:$SHARED_DIR:rw \
+    --env="XAUTHORITY=${XAUTH}" \
+    --env="DISPLAY=${DISPLAY}" \
+    --privileged -v /dev/bus/usb:/dev/bus/usb \
+    --net=host \
+    --name "fhtw_ros" \
+    georgno/fhtw-ros:mre2_ear_ws2023 python3 -m jupyterlab --ip='0.0.0.0' --no-browser
